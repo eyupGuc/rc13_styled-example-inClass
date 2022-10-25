7713a74717e5ee20fdfa518160ea48f496e33b01
eyupGuc <meydanguresi@gmail.com>
2022-10-25 22:03:59 +0300

    move

--- src/components/styles/Button.styled.jsx
+++ src/components/styles/Button.styled.jsx
@@ -8,6 +8,10 @@ const Button = styled.button`
   box-shadow: 0 0 10px rgba(0,0,0,0.15);
   font-size: 1.1rem;
   padding: 1rem 1.2rem;
+  font-size: 1.1rem;
+  margin-right: 2rem 0.5rem;
+  cursor: pointer;
+  &:hover{opacity: 0.9; transform:scale(0.97)}
 `;
 
 export default Button;
